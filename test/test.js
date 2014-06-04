311524413cfcb0134c6cc11d439cafb3c0a56b16
Carson Christian <cc@amplego.com>
2014-06-04 14:52:07 -0400

    don't require global link for tests

--- test/test.js
+++ test/test.js
@@ -31,7 +31,7 @@ describe('couchdb-merge', function() {
 	// Input checking.
 	describe('error conditions', function() {
 		it('should exit with status 1 if no database is provided', function(done) {
-			exec('couchdb-merge -j foo.json', function(e) {
+			exec('./index.js -j foo.json', function(e) {
 				should.exist(e);
 				e.should.be.instanceOf(Error);
 				e.code.should.equal(1);
@@ -39,7 +39,7 @@ describe('couchdb-merge', function() {
 			});
 		});
 		it('should exit with status 1 if no json file is provided', function(done) {
-			exec('couchdb-merge -d couchdb-merge-test', function(e) {
+			exec('./index.js -d couchdb-merge-test', function(e) {
 				should.exist(e);
 				e.should.be.instanceOf(Error);
 				e.code.should.equal(1);
@@ -47,7 +47,7 @@ describe('couchdb-merge', function() {
 			});
 		});
 		it('should exit with status 1 if json path is invalid', function(done) {
-			exec('couchdb-merge -d couchdb-merge-test -j bad/path.json', function(e) {
+			exec('./index.js -d couchdb-merge-test -j bad/path.json', function(e) {
 				should.exist(e);
 				e.should.be.instanceOf(Error);
 				e.code.should.equal(1);
@@ -55,7 +55,7 @@ describe('couchdb-merge', function() {
 			});
 		});
 		it('should exit with status 1 if json input is empty', function(done) {
-			exec('couchdb-merge -d couchdb-merge-test -j test/json/empty.json', function(e) {
+			exec('./index.js -d couchdb-merge-test -j test/json/empty.json', function(e) {
 				should.exist(e);
 				e.should.be.instanceOf(Error);
 				e.code.should.equal(1);
@@ -63,7 +63,7 @@ describe('couchdb-merge', function() {
 			});
 		});
 		it('should exit with status 1 if json input is malformed', function(done) {
-			exec('couchdb-merge -d couchdb-merge-test -j test/json/malformed.json', function(e) {
+			exec('./index.js -d couchdb-merge-test -j test/json/malformed.json', function(e) {
 				should.exist(e);
 				e.should.be.instanceOf(Error);
 				e.code.should.equal(1);
@@ -71,7 +71,7 @@ describe('couchdb-merge', function() {
 			});
 		});
 		it('should exit with status 1 if json input is not a root-level array', function(done) {
-			exec('couchdb-merge -d couchdb-merge-test -j test/json/object.json', function(e) {
+			exec('./index.js -d couchdb-merge-test -j test/json/object.json', function(e) {
 				should.exist(e);
 				e.should.be.instanceOf(Error);
 				e.code.should.equal(1);
@@ -79,7 +79,7 @@ describe('couchdb-merge', function() {
 			});
 		});
 		it('should exit with status 1 if target database does not exist', function(done) {
-			exec('couchdb-merge -d bad-db-name -j test/json/array-only.json', function(e) {
+			exec('./index.js -d bad-db-name -j test/json/array-only.json', function(e) {
 				should.exist(e);
 				e.should.be.instanceOf(Error);
 				e.code.should.equal(1);
@@ -91,19 +91,19 @@ describe('couchdb-merge', function() {
 	// No-ops.
 	describe('no-ops', function() {
 		it('should exit with status 0 when input json contains no objects', function(done) {
-			exec('couchdb-merge -d couchdb-merge-test -j test/json/array-only.json', function(e) {
+			exec('./index.js -d couchdb-merge-test -j test/json/array-only.json', function(e) {
 				should.not.exist(e);
 				done();
 			});
 		});
 		it('should exit with status 0 when input json contains only objects without valid _ids', function(done) {
-			exec('couchdb-merge -d couchdb-merge-test -j test/json/no-valid-ids.json', function(e) {
+			exec('./index.js -d couchdb-merge-test -j test/json/no-valid-ids.json', function(e) {
 				should.not.exist(e);
 				done();
 			});
 		});
 		it('should exit with status 0 when input json contains only objects with no matching couchdb documents', function(done) {
-			exec('couchdb-merge -d couchdb-merge-test -j test/json/no-matching.json', function(e) {
+			exec('./index.js -d couchdb-merge-test -j test/json/no-matching.json', function(e) {
 				should.not.exist(e);
 				done();
 			});
@@ -113,7 +113,7 @@ describe('couchdb-merge', function() {
 	// Merging.
 	describe('merging', function() {
 		it('should add new properties', function(done) {
-			exec('couchdb-merge -d couchdb-merge-test -j test/json/merge-add-new.json', function(e) {
+			exec('./index.js -d couchdb-merge-test -j test/json/merge-add-new.json', function(e) {
 				should.not.exist(e);
 				nano.use('couchdb-merge-test').get('test_document', function(e, doc) {
 					should.not.exist(e);
@@ -123,7 +123,7 @@ describe('couchdb-merge', function() {
 			});
 		});
 		it('should overwrite existing properties', function(done) {
-			exec('couchdb-merge -d couchdb-merge-test -j test/json/merge-overwrite.json', function(e) {
+			exec('./index.js -d couchdb-merge-test -j test/json/merge-overwrite.json', function(e) {
 				should.not.exist(e);
 				nano.use('couchdb-merge-test').get('test_document', function(e, doc) {
 					should.not.exist(e);
@@ -137,7 +137,7 @@ describe('couchdb-merge', function() {
 			});
 		});
 		it('should leave unmentioned properties alone', function(done) {
-			exec('couchdb-merge -d couchdb-merge-test -j test/json/merge-add-new.json', function(e) {
+			exec('./index.js -d couchdb-merge-test -j test/json/merge-add-new.json', function(e) {
 				should.not.exist(e);
 				nano.use('couchdb-merge-test').get('test_document', function(e, doc) {
 					should.not.exist(e);
